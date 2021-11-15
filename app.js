3366a125790d24efc192279730e3df20b6a97030
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 23:20:26 +0530

    mail api

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const mailjet = require('node-mailjet')
 .connect('51adbeea591a8cfc93cfad67bc690e3a',
  '3ff8fe7a682c89045372c90c303522f9')
 
-app.get("/", (req, res) => {
+app.get('/mail', (req,res) =>{
+    var msg = req.query.msg || 'Jikr Allah 1000 times daily';
+    var to= req.query.to || 'iqbalforall@gmail.com';
+    sendMail(to, msg);
+    res.send("send mail msg:" + msg + " to iqbalforall@gmail.com")
+});
 
-
+function sendMail(to, msg){
     const request = mailjet
     .post("send", {'version': 'v3.1'})
     .request({
@@ -18,24 +23,26 @@ app.get("/", (req, res) => {
                 "Name": "Shaik Ikbhal Basha"
             },
             "To": [{
-                "Email": "iqbalforall@gmail.com",
-                "Name": "Shaik Ikbhal Basha"
+                "Email": to||"iqbalforall@gmail.com",
+                "Name": "user"
             }],
             "Subject": "Read yaseen surah",
-            "TextPart": "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+            "TextPart": msg|| 'default yaseen mail messager: read yaseen daily',
             "HTMLPart": "Read yaseen surah daily"
         }]
-    })
-request
+    });
+    request
     .then((result) => {
         console.log(result.body)
     })
     .catch((err) => {
         console.log(err.statusCode)
     });
+}
 
-
-res.send("mail send to iqbalforall@gmail.com saying read yaseen , date now is " + Date.now());
+app.get("/", (req, res) => {
+    sendMail('iqbalforall@gmail.com', 'Read Quran daily ' + Date.now());
+    res.send("mail send to iqbalforall@gmail.com saying read yaseen , date now is " + Date.now());
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
